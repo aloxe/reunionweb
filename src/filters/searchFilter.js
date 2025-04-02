fcba15b7c944666465818d3beec855497f0e82ab
aloxe <github@blog.re>
2025-04-03 00:04:10 +0200

    research around search and remove async

--- src/filters/searchFilter.js
+++ src/filters/searchFilter.js
@@ -3,19 +3,19 @@ const elasticlunr = require("elasticlunr");
 module.exports = function (collection) {
   // what fields we'd like our index to consist of
   var index = elasticlunr(function () {
-    this.addField("title");
-    this.addField("description");
+    // this.addField("title");
+    // this.addField("description");
     this.addField("content");
     this.setRef("id");
   });
 
   // loop through each page and add it to the index
-  collection.forEach( async (page) => {
-    const frontMatter = await page.template.read(); // read is async since 11ty 3.0
+  collection.forEach( (page) => {
+    // const frontMatter = await page.template.read(); // read is async since 11ty 3.0
     index.addDoc({
       id: page.url,
-      title: frontMatter.data.title,
-      description: frontMatter.data.description,
+      // title: frontMatter.data.title,
+      // description: frontMatter.data.description,
       content: page.templateContent.replace(/<[^>]*>?|\r\n/gm, ' ').trim(),
     });
   });
