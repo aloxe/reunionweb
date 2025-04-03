6b6d3035a8f8fd711a488918d49bcb4177a55598
aloxe <github@blog.re>
2025-04-03 22:08:01 +0200

    rewrite searchIndex no grabe easier items

--- src/filters/searchFilter.js
+++ src/filters/searchFilter.js
@@ -3,20 +3,19 @@ const elasticlunr = require("elasticlunr");
 module.exports = function (collection) {
   // what fields we'd like our index to consist of
   var index = elasticlunr(function () {
-    // this.addField("title");
-    // this.addField("description");
+    this.addField("title");
+    this.addField("description");
     this.addField("content");
     this.setRef("id");
   });
 
   // loop through each page and add it to the index
   collection.forEach( (page) => {
-    // const frontMatter = await page.template.read(); // read is async since 11ty 3.0
     index.addDoc({
       id: page.url,
-      // title: frontMatter.data.title,
-      // description: frontMatter.data.description,
-      content: page.templateContent.replace(/<[^>]*>?|\r\n/gm, ' ').trim(),
+      title: page.data.title,
+      description: page.data.description,
+      content: page.data.page.rawInput.replace(/<[^>]*>?|\r\n/gm, ' ').trim(),
     });
   });
 
