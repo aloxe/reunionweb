469c48c17cf1a3fe3c16624170976da49a666395
aloxe <github@blog.re>
2025-07-24 13:31:28 +0200

    use sender that has good DMARK on dmain

--- src/layouts/includes/js/404email.js
+++ src/layouts/includes/js/404email.js
@@ -2,7 +2,7 @@ function makeNotice() {
   const url = window.location.href;
   const referer = document.referrer;
   // console.log(url);
-  sendEmail("404", "alx@lib.re", "\n404 not found on: \n" + url + "\n\n and referer = \n" + referer);
+  sendEmail("404", "alx@blog.re", "\n404 not found on: \n" + url + "\n\n and referer = \n" + referer);
 }
 
 function sendEmail(name, email, message) {
